b562fedb5eb8093eb49da1560f464da867ca9253
DeathsCookie <113200622+DeathsCookie@users.noreply.github.com>
2022-12-05 08:44:22 +0000

    nital Upload

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
